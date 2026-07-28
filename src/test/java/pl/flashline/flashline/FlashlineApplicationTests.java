e47c76c876cedc91327286dccb80c61070f925cc
krzys <krzysztofmuller1@gmail.com>
2026-07-29 01:04:08 +0200

    init springboot

---
+++ src/test/java/pl/flashline/flashline/FlashlineApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.flashline.flashline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
